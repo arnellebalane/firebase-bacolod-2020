cd64ec178da5ef2ea6c456b8cc405adb95c9e820
Arnelle Balane <arnellebalane@gmail.com>
2020-03-06 21:06:45 +0800

    Initialize Firebase app

---
+++ javascripts/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBytq5Bza8qDUBLJvgf1Smh-8m9mPIRz4c',
+  authDomain: 'vue-firebase-todo-app-b4136.firebaseapp.com',
+  databaseURL: 'https://vue-firebase-todo-app-b4136.firebaseio.com',
+  projectId: 'vue-firebase-todo-app-b4136',
+  storageBucket: 'vue-firebase-todo-app-b4136.appspot.com',
+  messagingSenderId: '957118775204',
+  appId: '1:957118775204:web:f98dbc35d8f0cfe4e7cb31'
+};
+
+firebase.initializeApp(firebaseConfig);
